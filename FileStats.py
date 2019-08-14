36273d0c22630565ad7d28d1cc6bf701410477ca
Michael Pascale <32907334+MichaelPascale@users.noreply.github.com>
2019-08-14 16:04:03 -0400

    Add files via upload

---
+++ FileStats.py
@@ -0,0 +1,23 @@
+'''
+    FileStats.py
+    Counts added lines of code in added in a directory of patch files.
+    
+    Copyright 2019, Michael Pascale
+'''
+
+import os
+
+files = os.listdir()
+
+i = 0
+
+for file in files:
+    with open(file) as f:
+        for line in f:
+            if line[0] is "+":
+                if line[1:3] != "++":              
+                    i += 1
+
+print(i)
+    
+
